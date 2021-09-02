68a2f716cc3aa9a8b01f96aff7c62fcb75c68f62
Oleg-Khakhuda <buddy2317@gmail.com>
2021-09-02 21:58:44 +0300

    Добавляет стили

--- main.js
+++ main.js
@@ -6,28 +6,18 @@ refs = {
     form: document.querySelector('.todo__input'),
 }
 
-// refs.inputText.addEventListener('input', add)
-refs.form.addEventListener('click', text)
-
-function text(e) {
-    const value = refs.inputText.value
-    console.log(value);
-}
-
-
-
 function init() {
-    const fromStorage = localStorage.getItem('todo');
+  const fromStorage = localStorage.getItem('todo');
     if (fromStorage) {
-        refs.getLocalStorage.innerHTML = fromStorage
+      refs.getLocalStorage.innerHTML = fromStorage;
     }
-    refs.todoOptions.addEventlistner('change', this.update);
-    document.addEventListener('click', this.action.bing(this))
+    refs.todoOptions.addEventListener('click', update);
+    document.addEventListener('click', action)
 }
 
 function create(text) {
     return `<li class="todo__item" data-todo-state="active">
-    <span class="todo__task">${text}</span>
+    <span class="todo__task">${num}. ${text}</span>
     <span class="todo__action todo__action_restore" data-todo-action="active"></span>
     <span class="todo__action todo__action_complete" data-todo-action="completed"></span>
     <span class="todo__action todo__action_delete" data-todo-action="deleted"></span></li>`;
@@ -39,22 +29,21 @@ function save() {
 }
 
 function update() {
-    const options = refs.todoOptions.value;
+    const option = refs.todoOptions.value;
     refs.getLocalStorage.dataset.todoOption = option;
     refs.inputText.disabled = option !== 'active'
 }
 
 function add() {
-    // const elemText = document.querySelector('.todo__text');
   if (refs.inputText.disabled || !refs.inputText.value.length) {
     return;
   }
-  refs.getLocalStorage.insertAdjacentHTML('beforeend', this.create(refs.inputText.value));
+  refs.getLocalStorage.insertAdjacentHTML('beforeend', create(refs.inputText.value));
   refs.inputText.value = '';
 }
 
 function action(e) {
-    const target = e.target;
+  const target = e.target;
   if (target.classList.contains('todo__action')) {
     const action = target.dataset.todoAction;
     const elemItem = target.closest('.todo__item');
@@ -63,9 +52,11 @@ function action(e) {
     } else {
       elemItem.dataset.todoState = action;
     }
-    this.save();
+    save();
   } else if (target.classList.contains('todo__add')) {
-    this.add();
-    this.save();
+    add();
+    save();
   }
 }
+
+init();
